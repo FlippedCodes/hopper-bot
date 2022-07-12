1bf16781fc7c73c9e57b264512cab0ed5ff7dab6
Phil <philip.schaefer0@gmail.com>
2022-07-13 01:59:02 +0200

    [hotfix] diable status

--- functions/SETUP/status.js
+++ functions/SETUP/status.js
@@ -1,12 +1,9 @@
 module.exports.run = async () => {
   if (DEBUG) return;
+  return;
   console.log(`[${module.exports.data.name}] Setting status...`);
   await client.user.setStatus('online');
   await client.user.setActivity(`with ${client.users.cache.size}`, { type: 'PLAYING' });
-  // TODO: sharing version
-  // const guildCountsArr = await client.shard.fetchClientValues('guilds.cache.size');
-  // const guildCounts = guildCountsArr.reduce((previousCount, currentCount) => previousCount + currentCount, 0);
-  // await client.user.setActivity(`${guildCounts} servers.`, { type: 'LISTENING' });
   console.log(`[${module.exports.data.name}] Status set!`);
 };
 
