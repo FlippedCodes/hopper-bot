ac7a9cba9169d033549a86e19d7571f1597c9780
Phil <philip.schaefer0@gmail.com>
2022-11-25 18:53:41 +0100

    [update] removed temp code

--- index.js
+++ index.js
@@ -63,10 +63,6 @@ client.on('ready', async () => {
   // confirm user logged in
   console.log(`[${config.package.name}] Logged in as "${client.user.tag}"!`);
 
-  // TEMP
-  console.log(client.guilds.cache.size);
-  console.log(client.guilds.cache);
-
   // run setup functions
   config.setup.setupFunctions.forEach((FCN) => {
     client.functions.get(FCN).run();
